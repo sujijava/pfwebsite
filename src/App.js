7739a87715bdb49ce2a6ace390652e869c5624b9
sujijava <59837559+sujijava@users.noreply.github.com>
2021-04-10 01:14:54 -0400

    confetti-windowSize

--- src/App.js
+++ src/App.js
@@ -3,25 +3,41 @@ import Header from './components/Header'
 import About from './components/About'
 import Resume from './components/Resume'
 import Portfolio from './components/Portfolio'
-import ContactUs from './components/ContactUs'
 import Footer from './components/Footer'
 import resumeData from './resumeData'
 import Confetti from 'react-confetti'
+import useWindowDimensions from './components/useWindowDimensions'
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Confetti width='2000' height='1000' />
-        <Header resumeData={resumeData} />
-        <About resumeData={resumeData} />
-        <Resume resumeData={resumeData} />
-        <Portfolio resumeData={resumeData} />
-        {/* <ContactUs resumeData={resumeData} /> */}
-        <Footer resumeData={resumeData} />
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className='App'>
+      <Confetti />
+      <Header resumeData={resumeData} />
+      <About resumeData={resumeData} />
+      <Resume resumeData={resumeData} />
+      <Portfolio resumeData={resumeData} />
+      {/* <ContactUs resumeData={resumeData} /> */}
+      <Footer resumeData={resumeData} />
+    </div>
+  )
 }
 
 export default App
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className='App'>
+//         <Confetti />
+//         <Header resumeData={resumeData} />
+//         <About resumeData={resumeData} />
+//         <Resume resumeData={resumeData} />
+//         <Portfolio resumeData={resumeData} />
+//         {/* <ContactUs resumeData={resumeData} /> */}
+//         <Footer resumeData={resumeData} />
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
