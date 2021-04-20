44046f29c1b2a7189a02820dd7e4eedad316df74
Susie <susieoh8@gmail.com>
2021-04-20 18:09:43 -0400

    Update resumeData.js

--- src/resumeData.js
+++ src/resumeData.js
@@ -33,7 +33,7 @@ let resumeData = {
   ],
   work: [
     {
-      CompanyName: 'Freelance Web Developer',
+      CompanyName: 'Dating App Proejct',
       specialization: 'Front-end Web Development (React)',
       MonthOfLeaving: 'March',
       YearOfLeaving: '2021~',
@@ -54,6 +54,22 @@ let resumeData = {
     },
   ],
   portfolio: [
+    {
+      name: 'Bay Shop',
+      description: 'A e-commerce web site with shopping cart',
+      imgurl: 'images/projects/bay_shop.jpg',
+      live_demo_url: 'https://bay-shop.herokuapp.com/',
+      source_code_url: 'https://github.com/sujijava/bay-shop-deploy',
+      skills: [
+        'devicon-react-original colored',
+        'devicon-mongodb-plain-wordmark colored',
+        'devicon-nodejs-plain-wordmark colored',
+        'devicon-express-original-wordmark colored',
+        'devicon-html5-plain colored',
+        'devicon-css3-plain-wordmark colored',
+        'devicon-bootstrap-plain-wordmark colored',
+      ],
+    },
     {
       name: 'Favorite Movie Picker',
       description: 'A web application for saving favorite movie list',
